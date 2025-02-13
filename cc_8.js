fbd4b7c9e9ea47c253bca1a0c2f1900d42d7eecd
Maidele Archelus <maidele@usf.edu>
2025-02-13 13:19:30 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -1,4 +1,4 @@
-//Task 1
+//Task 1- Employee Salary Calculation; Function Declarations 
 
 function calculatenetSalary(baseSalary, bonus, taxRate) {
     let netSalary =(baseSalary + bonus) - (baseSalary * taxRate);
@@ -6,6 +6,20 @@ function calculatenetSalary(baseSalary, bonus, taxRate) {
 
 }
 
-// Test Cases
+// Test Data
 calculatenetSalary(5000, 500, 1.); // Expected output: "Net Salary: $5000.00"
 calculatenetSalary(7000, 1000, .15); // Expected output: "Net Salary: $6950.00"
+
+
+//Task 2-
+
+const calculateDiscount = function(price, discountRate) {
+    let finalPrice = price - (price * discountRate);
+    console.log(`Final Price: $${finalPrice}`);
+};
+
+// Test Data
+calculateDiscount(100, 0.2); // Expected output: "Final Price: $80.00"
+calculateDiscount(250, 0.15); // Expected output: "Final Price: $212.50"
+
+
