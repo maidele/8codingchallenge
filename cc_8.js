d14754e2b36cb1fb9634f5366c3624a3dc8ac087
Maidele Archelus <maidele@usf.edu>
2025-02-15 10:52:31 -0500

    Task 8 - Savings Growth Projection

--- cc_8.js
+++ cc_8.js
@@ -83,3 +83,14 @@ function createCartTracker() {
  let cart = createCartTracker();
  console.log(cart(20)); // Expected output: "Total Cart Value: $20"
  console.log(cart(35)); // Expected output: "Total Cart Value: $55"
+
+ //Task 8
+function calculateSavings(years, amount) {
+    if (years == 10) return amount;
+    return calculateSavings(amount + .05);
+ }
+ 
+ 
+ // Test Cases
+ calculateSavings(8, 1000); // Expected output: "Projected Savings: $1102.50"
+ calculateSavings(5, 5000); // Expected output: "Projected Savings: $5525.63"
